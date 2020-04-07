dfe209b67cba714537b916e62a327fff41ec80d5
xinulojahu <kashkarev154@gmail.com>
2020-04-08 02:03:08 +0700

    Added texts_read function

---
+++ texts.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TEXT_COUNT 10
+#define MAX_SYMBOL_COUNT 3072
+
+char** texts_read() {
+    //Инициализация переменных
+    FILE* texts_file;  //Для работы с файлом
+    char* str;         //Для считывания строк
+    int text_number;   //Номер текста
+    fpos_t pos;  //Для того, чтобы запомнить позицию в файле
+    int newline_count;  //Количество переходов на новую строку
+    char** text_out;  //Хранит текст под номером text_number
+
+    //Объявление переменных
+    texts_file = fopen("RU/texts.txt", "r");  //Открытие файла
+    if (texts_file == NULL) {  //Проверка, открылся ли файл
+        return NULL;
+    }
+    str = malloc(MAX_SYMBOL_COUNT *
+                 sizeof(char));  //выделение памяти для хранения строк
+    if (str == NULL) {  //Выделилась ли память
+        return NULL;
+    }
+    srand(time(NULL));                  //Случайный сид
+    text_number = rand() % TEXT_COUNT;  //Открытия случайного текста
+    newline_count = 0;  //Обнуление счетчика переходов на новую строку
+
+    //Поиск нужного текста
+    while (text_number) {
+        fgets(str, MAX_SYMBOL_COUNT, texts_file);
+        if ((str[0] == '&') && (str[1] == '&')) {
+            text_number--;
+        }
+    }
+
+    //Подсчет количества переходов на новую строку
+    fgetpos(texts_file, &pos);
+    do {
+        newline_count++;
+        fgets(str, MAX_SYMBOL_COUNT, texts_file);
+    } while ((str[0] != '&') || (str[1] != '&'));
+    fsetpos(texts_file, &pos);
+
+    //Выделения памяти. Каждому абзацу своя переменная.
+    text_out = (char**)malloc(newline_count * sizeof(char*));
+    if (text_out == NULL) {
+        return NULL;
+    }
+
+    //Считывание текста в память
+    for (int i = 0; i < newline_count; i++) {
+        text_out[i] = malloc(MAX_SYMBOL_COUNT * sizeof(char));
+        fgets(text_out[i], MAX_SYMBOL_COUNT, texts_file);
+    }
+
+    //освобождение
+    free(str);
+    fclose(texts_file);
+
+    //Возвращаем указатель на наш текст
+    return text_out;
+}
+
+int main() {
+    //Проверка работоспособности
+    char** texts;
+    texts = texts_read();
+    for (int i = 0; i < 2; i++) {
+        puts(texts[i]);
+    }
+    return 0;
+}
