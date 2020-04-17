c8e6867a5db23dfce3a552bab622730951ae8cc2
cah9 <sibsutis.gerasimov@gmail.com>
2020-04-17 19:00:21 +0700

    Add: print stats function

---
+++ stats.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int stats()
+{
+    FILE* stats_file;
+    char str;
+    stats_file = fopen("results.txt", "r");
+    if (stats_file == NULL) {
+        printf("Error openning the file!\n");
+        return -1;
+    }
+
+    str = fgetc(stats_file);
+    while (str != EOF) {
+        putchar(str);
+        str = getc(stats_file);
+    }
+    printf("\n");
+    fclose(stats_file);
+
+    return 0;
+}
+
+int main()
+{
+    stats();
+    return 0;
+}
