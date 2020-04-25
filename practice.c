af5e6598a0378c4d2674c05d9cbea6a57f62af18
Joletx42 <alek.solov379@gmail.com>
2020-04-25 11:53:30 +0700

    Now practice.c can return double figures

--- practice.c
+++ practice.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 int practice();  //основная фукнция
-int char_to_int(char*, int);  // функция перевода числв char в число int
+int char_to_int(char*, int, int);  // функция перевода числв char в число int
 
-int char_to_int(char* str, int i) {
+int char_to_int(char* str, int i, int j) {
     int a;
+    int b;
     a = str[i] - '0';
+    b = str[j] - '0';
+    if (b > 0) {
+        b *= 10;
+        a += b;
+    }
     return a;
 }
 
@@ -27,8 +33,8 @@ int practice() {
 
     str = malloc(3000 * sizeof(char));  // выделяем динамическую память для str
     fgets(str, 150, practice_done);  // записываем в массив первую строку
-    min = char_to_int(str, 18);  // запоминаем минимальным количеством
-                                 // попыток количество в первом тексте
+    min = char_to_int(str, 18, 17);  // запоминаем минимальным количеством
+                                     // попыток количество в первом тексте
     start_min = min;  // присваеваем для дальнейшего сравнения
 
     for (int i = 1; !feof(practice_done); i++) {
@@ -36,7 +42,7 @@ int practice() {
             str, 150,
             practice_done);  //получаем строку (уже получаем вторую и так далее)
         if (i % 3 == 0) {  // если строка с количеством попыток
-            temp = char_to_int(str, 18);  //то запоминаем эти попытки в temp
+            temp = char_to_int(str, 18, 17);  //то запоминаем эти попытки в temp
             key++;  // повышаем количество текстов в файле
             if (temp < min) {  // если temp больше min
                 min = temp;    // присваемваем min temp
