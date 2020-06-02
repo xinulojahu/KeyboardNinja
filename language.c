b43a715ef0967c98203cb2af5eda6fa2803747e4
xinulojahu <kashkarev154@gmail.com>
2020-06-03 00:38:07 +0700

    Add get_language func

--- language.c
+++ language.c
@@ -5,3 +5,29 @@
 #define MAX_SIZE 64
 
 char language[3] = "EN";
+
+int get_language() {
+    FILE* settings_file;
+    settings_file = fopen("settings", "r");
+    if (settings_file == NULL) {
+        return -1;
+    }
+    char* str = malloc(MAX_SIZE * sizeof(char));
+    while (fgets(str, MAX_SIZE, settings_file) != NULL) {
+        char* cur = strtok(str, "=");
+        if (cur != NULL) {
+            if (strcmp(cur, "language") == 0) {
+                cur = strtok(NULL, "=");
+                if ((strcmp(cur, "RU") == 0) || (strcmp(cur, "EN") == 0)) {
+                    strcpy(language, cur);
+                    fclose(settings_file);
+                    free(str);
+                    return 0;
+                }
+            }
+        }
+    }
+    free(str);
+    fclose(settings_file);
+    return -1;
+}
