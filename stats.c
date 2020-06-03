136fae213fd4a6685e2cac47b8b80781f1f2b130
xinulojahu <kashkarev154@gmail.com>
2020-06-04 04:11:00 +0700

    Add change language to stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "language.h"
 
 int stats() {
     FILE* stats_file;
     char str;
-    stats_file = fopen("results.txt", "r");
+    char* language[3];
+    get_language(language);
+
+    char* path = malloc(32 * sizeof(char));
+    sprintf(path, "%s/results.txt", language);
+    stats_file = fopen(path, "r");
+    free(path);
     if (stats_file == NULL) {
         printf("Error openning the file!\n");
         return -1;
