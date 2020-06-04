f0b69331b4f538166f259c3c0cdd03365ab98b03
xinulojahu <kashkarev154@gmail.com>
2020-06-05 01:30:31 +0700

    Add stats_export func

--- stats.c
+++ stats.c
@@ -3,9 +3,9 @@
 #include "language.h"
 #include "practice.h"
 
-#define ST_PRINT                                                               \
-    fprintf(stats_file, "%03d|%02d:%02d|%4d|%3d|%5.1f%%\n", num, seconds / 60, \
-            seconds % 60, sym_per_minuts, errors, errors_prcnt);
+#define ST_PRINT                                                    \
+    fprintf(stats_file, "%03d|%02d:%02d|%04d|%03d|%05.1f%%\n", num, \
+            seconds / 60, seconds % 60, sym_per_minuts, errors, errors_prcnt);
 
 int stats_fprint(int seconds, int sym_per_minuts, int errors,
                  double errors_prcnt) {
@@ -24,9 +24,9 @@ int stats_fprint(int seconds, int sym_per_minuts, int errors,
     temp_file = fopen("res_temp.txt", "w+");
 
     fpos_t pos;
-    char* str;
     int num = 1;
     int best_spm = 0;
+    char* str;
     str = malloc(64 * sizeof(char));
 
     if (fgets(str, 64, stats_file) != NULL) {
@@ -38,12 +38,9 @@ int stats_fprint(int seconds, int sym_per_minuts, int errors,
             }
             cur++;
         }
-        while (*cur == ' ') {
-            cur++;
-        }
-        best_spm = char_to_int(cur, 0, 1, 2);
+        best_spm = char_to_int(cur, '|');
         fgets(str, 64, stats_file);
-        num = (char_to_int(str, 0, 1, 2) + 1) % 1000;
+        num = (char_to_int(str, '|') + 1) % 1000;
         fputs(str, temp_file);
     }
     while (fgets(str, 64, stats_file) != NULL) {
@@ -62,6 +59,64 @@ int stats_fprint(int seconds, int sym_per_minuts, int errors,
     remove("res_temp.txt");
     fclose(temp_file);
     fclose(stats_file);
+    free(str);
+    return 0;
+}
+
+int stats_export() {
+    FILE* stats_file;
+    char language[3];
+    get_language(language);
+    char* path = malloc(32 * sizeof(char));
+    sprintf(path, "%s/results.txt", language);
+    stats_file = fopen(path, "r");
+    if (stats_file == NULL) {
+        printf("Результатов еще нет");
+        return -1;
+    }
+    free(path);
+    char* str;
+    str = malloc(64 * sizeof(char));
+
+    fgets(str, 64, stats_file);
+    FILE* ploterror;
+    FILE* plotspm;
+    ploterror = fopen("gnuplot_error.txt", "w");
+    plotspm = fopen("gnuplot_spm.txt", "w");
+
+    int spm;
+    int num;
+    double error;
+    char* cur;
+    while (fgets(str, 64, stats_file) != NULL) {
+        num = char_to_int(str, '|');
+        cur = str;
+        for (int i = 0; i < 2; i++) {
+            while (*cur != '|') {
+                cur++;
+            }
+            cur++;
+        }
+        spm = char_to_int(cur, '|');
+        for (int i = 0; i < 2; i++) {
+            while (*cur != '|') {
+                cur++;
+            }
+            cur++;
+        }
+        error = char_to_int(cur, ',');
+        while (*cur != ',') {
+            cur++;
+        }
+        cur++;
+        error += char_to_int(cur, '%') * 0.1;
+        fprintf(plotspm, "%d %d\n", num, spm);
+        fprintf(ploterror, "%d %.1lf\n", num, error);
+    }
+    fclose(ploterror);
+    fclose(plotspm);
+    fclose(stats_file);
+    free(str);
     return 0;
 }
 
