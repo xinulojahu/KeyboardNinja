d5d5a3ec1e94d19e0cc5d402d4e1bb90918149c0
xinulojahu <kashkarev154@gmail.com>
2020-06-05 00:37:19 +0700

    Add best result

--- stats.c
+++ stats.c
@@ -3,6 +3,10 @@
 #include "language.h"
 #include "practice.h"
 
+#define ST_PRINT                                                               \
+    fprintf(stats_file, "%03d|%02d:%02d|%4d|%3d|%5.1f%%\n", num, seconds / 60, \
+            seconds % 60, sym_per_minuts, errors, errors_prcnt);
+
 int stats_fprint(int seconds, int sym_per_minuts, int errors,
                  double errors_prcnt) {
     FILE* stats_file;
@@ -19,20 +23,39 @@ int stats_fprint(int seconds, int sym_per_minuts, int errors,
     FILE* temp_file;
     temp_file = fopen("res_temp.txt", "w+");
 
+    fpos_t pos;
     char* str;
     int num = 1;
+    int best_spm = 0;
     str = malloc(64 * sizeof(char));
 
     if (fgets(str, 64, stats_file) != NULL) {
+        fgetpos(stats_file, &pos);
+        char* cur = str;
+        for (int i = 0; i < 2; i++) {
+            while (*cur != '|') {
+                cur++;
+            }
+            cur++;
+        }
+        while (*cur == ' ') {
+            cur++;
+        }
+        best_spm = char_to_int(cur, 0, 1, 2);
+        printf("\n ---- %d\n", best_spm);
+        fgets(str, 64, stats_file);
         num = (char_to_int(str, 0, 1, 2) + 1) % 1000;
         fputs(str, temp_file);
     }
     while (fgets(str, 64, stats_file) != NULL) {
         fputs(str, temp_file);
     }
-    fseek(stats_file, 0, SEEK_SET);
-    fprintf(stats_file, "%03d|%02d:%02d|%4d|%3d|%5.1f%%\n", num, seconds / 60,
-            seconds % 60, sym_per_minuts, errors, errors_prcnt);
+    if (best_spm < sym_per_minuts) {
+        fseek(stats_file, 0, SEEK_SET);
+        ST_PRINT;
+    }
+    fsetpos(stats_file, &pos);
+    ST_PRINT;
     fseek(temp_file, 0, SEEK_SET);
     while (fgets(str, 64, temp_file) != NULL) {
         fputs(str, stats_file);
