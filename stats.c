616b42a7c47212e023b09d2a6c0aa5658891d8e6
xinulojahu <kashkarev154@gmail.com>
2020-06-05 00:35:21 +0700

    Add output stats at file

--- stats.c
+++ stats.c
@@ -16,18 +16,29 @@ int stats_fprint(int seconds, int sym_per_minuts, int errors,
     }
     free(path);
 
-    fpos_t pos;
+    FILE* temp_file;
+    temp_file = fopen("res_temp.txt", "w+");
+
     char* str;
     int num = 1;
     str = malloc(64 * sizeof(char));
 
-    fgetpos(stats_file, &pos);
     if (fgets(str, 64, stats_file) != NULL) {
-        num = char_to_int(str, 0, 1, 2);
+        num = (char_to_int(str, 0, 1, 2) + 1) % 1000;
+        fputs(str, temp_file);
     }
-    fsetpos(stats_file, &pos);
-    fprintf(stats_file, "%d|%02d:%02d|%d|%d|%4.1f%%\n", num, seconds / 60,
+    while (fgets(str, 64, stats_file) != NULL) {
+        fputs(str, temp_file);
+    }
+    fseek(stats_file, 0, SEEK_SET);
+    fprintf(stats_file, "%03d|%02d:%02d|%4d|%3d|%5.1f%%\n", num, seconds / 60,
             seconds % 60, sym_per_minuts, errors, errors_prcnt);
+    fseek(temp_file, 0, SEEK_SET);
+    while (fgets(str, 64, temp_file) != NULL) {
+        fputs(str, stats_file);
+    }
+    remove("res_temp.txt");
+    fclose(temp_file);
     fclose(stats_file);
     return 0;
 }
