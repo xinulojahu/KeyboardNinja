41ddb02b6a1f2e62943de845fe2ec2778b91de16
cah9 <sibsutis.gerasimov@gmail.com>
2020-06-05 16:53:41 +0700

    Add comments in practice.c

--- practice.c
+++ practice.c
@@ -1,13 +1,14 @@
+#include "language.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "language.h"
 
 // int practice();  //основная фукнция
 // int char_to_int(char*, int, int);  // функция перевода числв char в число int
 // int practice_print(int); // Функция вывода лучших результатов
 
-int char_to_int(char* str, int i, int j, int k) {
+int char_to_int(char* str, int i, int j, int k)
+{
     int a;
     int b = str[j];
     int c = str[k];
@@ -27,12 +28,13 @@ int char_to_int(char* str, int i, int j, int k) {
     return a;
 }
 
-int practice_print(int num) {
-    FILE* practice_best;
-    char str[100];
-    char *estr;
+int practice_print(int num)
+{
+    FILE* practice_best; //Переменная с указателем на поток данных
+    char str[100]; //Переменная для записи строк
+    char* estr; //Указатель с адресом массива со строкой
     int numcount;
-    int count = 1;
+    int count = 1; //Счетчик итераций цикла
     practice_best = fopen("practice_done.txt", "r");
     numcount = num;
     num = num - 1;
@@ -42,26 +44,25 @@ int practice_print(int num) {
     }
     //Чтение построчно данных из файла в бесконечном цикле
     while (1) {
-            // Чтение одной строки  из файла
-            estr = fgets (str, 100, practice_best);
-            //Проверка на конец файла или конец аргумента
-            if ((estr == NULL) || (num == 0)) {
-                if (feof (practice_best) != 0) {
-                    printf("Reading is complete!\n");
-                    break;
-                }
-                else {
-                    printf("Reading is complete.\n");
-                    break;
-                }
+        // Чтение одной строки  из файла
+        estr = fgets(str, 100, practice_best);
+        //Проверка на конец файла или конец аргумента
+        if ((estr == NULL) || (num == 0)) {
+            if (feof(practice_best) != 0) {
+                printf("Reading is complete!\n");
+                break;
+            } else {
+                printf("Reading is complete.\n");
+                break;
             }
-            num = num - 1;
-            count++;
+        }
+        num = num - 1; //Уменьшение аргумента с каждой итерацией
+        count++; //Счетчик
     }
-    if ((count == numcount) && (feof (practice_best) == 0)) {
-        puts(str);
-    }
-    else {      
+    //Условие на проверку конца цикла или конец файла
+    if ((count == numcount) && (feof(practice_best) == 0)) {
+        puts(str); //Вывод нужной строки
+    } else {
         printf("Invalid input!\n");
     }
     //Закрытие файла
@@ -70,52 +71,53 @@ int practice_print(int num) {
     return 0;
 }
 
-int practice() {
+int practice()
+{
     char language[3];
     get_language(language);
     FILE* practice_done;
-    char* str;  // указатель на массив строк из файла
-    int min;    // минимальное количество попыток
-    int start_min;  // используется для финальной проверки
-    int temp;  // переменная для записи количества попыток для сравнения
-    int key = 1;  // переменная для запоминания номера текста
-    int number_of_text = 0;  // переменная, в которой хранится
-                             // необходимый номер текста
-    char* path = malloc(32 * sizeof(char));  //массив для пути
-    sprintf(path, "%s/practice_done.txt", language);  //создает текстовый путь
-    practice_done = fopen(path, "r");  // открываем файл
-    free(path);  //особождаем массив для пути
-    if (practice_done == NULL) {  // проверка на открытие файла
+    char* str; // указатель на массив строк из файла
+    int min;   // минимальное количество попыток
+    int start_min; // используется для финальной проверки
+    int temp; // переменная для записи количества попыток для сравнения
+    int key = 1; // переменная для запоминания номера текста
+    int number_of_text = 0; // переменная, в которой хранится
+                            // необходимый номер текста
+    char* path = malloc(32 * sizeof(char)); //массив для пути
+    sprintf(path, "%s/practice_done.txt", language); //создает текстовый путь
+    practice_done = fopen(path, "r"); // открываем файл
+    free(path); //особождаем массив для пути
+    if (practice_done == NULL) { // проверка на открытие файла
         printf("Error with opening file");
         return -1;
     }
 
-    str = malloc(152 * sizeof(char));  // выделяем динамическую память для str
-    fgets(str, 150, practice_done);  // записываем в массив первую строку
-    min = char_to_int(str, 0, 1, 2);  // запоминаем минимальным количеством
-                                      // попыток количество в первом тексте
-    start_min = min;  // присваеваем для дальнейшего сравнения
+    str = malloc(152 * sizeof(char)); // выделяем динамическую память для str
+    fgets(str, 150, practice_done); // записываем в массив первую строку
+    min = char_to_int(str, 0, 1, 2); // запоминаем минимальным количеством
+                                     // попыток количество в первом тексте
+    start_min = min; // присваеваем для дальнейшего сравнения
 
     for (int i = 1; !feof(practice_done); i++) {
-        fgets(str, 150,
-              practice_done);  //получаем строку (уже получаем вторую и так
-                               //далее)
-        if (i % 2 == 0) {  // если строка с количеством попыток
-            temp =
-                char_to_int(str, 0, 1, 2);  //то запоминаем эти попытки в temp
-            key++;  // повышаем количество текстов в файле
-            if (temp < min) {  // если temp больше min
-                min = temp;    // присваемваем min temp
-                number_of_text = key;  // запоминаем номер необзодимого текста
+        fgets(str,
+              150,
+              practice_done); //получаем строку (уже получаем вторую и так
+                              //далее)
+        if (i % 2 == 0) { // если строка с количеством попыток
+            temp = char_to_int(str, 0, 1, 2); //то запоминаем эти попытки в temp
+            key++; // повышаем количество текстов в файле
+            if (temp < min) { // если temp больше min
+                min = temp;   // присваемваем min temp
+                number_of_text = key; // запоминаем номер необзодимого текста
             }
         }
     }
 
-    if (min == start_min) {  // если минимальное количество осталось прежним
-        number_of_text = 1;  // запоминаем первый текст
+    if (min == start_min) { // если минимальное количество осталось прежним
+        number_of_text = 1; // запоминаем первый текст
     }
-    fclose(practice_done);  // закрываем файл
+    fclose(practice_done); // закрываем файл
     free(str);
 
-    return number_of_text;  // возвращаем номер текста
+    return number_of_text; // возвращаем номер текста
 }
