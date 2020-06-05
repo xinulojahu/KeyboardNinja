772f2fe848ae9fec51b91e43601f214fd65589db
cah9 <sibsutis.gerasimov@gmail.com>
2020-06-05 16:53:14 +0700

    Add comments in menu.c

--- menu.c
+++ menu.c
@@ -1,12 +1,12 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <wchar.h>
 #include <string.h>
+#include <wchar.h>
 //#include "practice.h"
 //#include "texts.h"
 #include <locale.h>
-
+//Функция для вывода главного меню
 void menu_display()
 {
     printf(" _____ _____ _____ _____ _____ _____ _____ _____ __________ ");
@@ -42,117 +42,121 @@ void menu_display()
 
 void menu(void)
 {
-    char str[100]; //Входной поток данных
-    char choice; //Переменная для свича
+    char str[100];      //Входной поток данных
+    char choice;        //Переменная для свича
     char sep[10] = " "; //Значение передаваемое в strtok
-    char *istr; //Тот же же strtok
-    char *istr1; //Все еще strtok
+    char* istr;         //Тот же же strtok
+    char* istr1;        //Все еще strtok
     char istr2[100]; //Вы угадали, это все что я умею :/
     int arg = 0; //Интовый аргумент посылаемый в функцию
     int argtest = 0; //Переменная для аргумента
     do {
-        //fgets(str, 100, stdin);
-        gets(str);
-        strncpy(istr2, str, 20);
-        istr1 = strtok(istr2, sep);
-        
-        printf("Your choice it's; %s\n", istr1); 
-        istr = strtok(str, sep);
-        
-        while (istr != NULL) {            
-            istr = strtok (NULL, sep);
+        // fgets(str, 100, stdin);
+        gets(str);               //Считываем строку
+        strncpy(istr2, str, 20); //Копируем содержимое str в str2
+        istr1 = strtok(istr2, sep); //Вычленяем комманду из строки
+
+        printf("Your choice it's; %s\n", istr1); //Вспомогательная строка
+        istr = strtok(str, sep); //Выделение первой части строки
+
+        //Цикл для выделения следующей части (аргумента)
+        while (istr != NULL) {
+            istr = strtok(NULL, sep);
             break;
         }
-        //printf("Argument it's %s\n", istr);
+        // printf("Argument it's %s\n", istr);
+        //Сравнение содержимого строки с командой
         if (strcmp(istr1, "test") == 0) {
-            choice = '1';
-            printf("1\n");
+            choice = '1'; //Присвоение соответствующего выбора для свичкейса
+            printf("1\n"); //Вспомогательная строка
         }
+        //Сравнение содержимого строки с командой
         else if (strcmp(istr1, "practice") == 0) {
-            if ((istr != NULL) && (isnumber(istr) == 0)) { 
-                argtest = atoi(istr);
-                if ((argtest >= 1) && (argtest <= 20)) {
-                    arg = argtest;
-                    printf("Argument it's; %d\n", arg);
+            if ((istr != NULL) && (isnumber(istr) == 0)) {
+                argtest = atoi(istr); //Вспомогательная переменная для аргумента
+                if ((argtest >= 1) && (argtest <= 20)) { //Условие на аргумент
+                    arg = argtest; //Конечный аргумент
+                    printf("Argument it's; %d\n", arg); //Вспомогательная строка
                 } else {
-                    printf("Invalid argument!\n");
+                    printf("Invalid argument!\n"); //Неверный аргумент
                 }
-                
             }
-            choice = '2';
-            printf("2\n");
+            choice = '2'; //Присвоение соответствующего выбора для свичкейса
+            printf("2\n"); //Вспомогательная строка
         }
+        //Сравнение содержимого строки с командой
         else if (strcmp(istr1, "practice_best") == 0) {
-            choice = '3';
-            printf("3\n");
+            choice = '3';  //Для свичкейса
+            printf("3\n"); //Вспомогательная строка
         }
+        //Сравнение содержимого строки с командой
         else if (strcmp(istr1, "language") == 0) {
-            choice = '4';
-             if (istr != NULL) {                
-                    if (strcmp(istr, "EN") == 0) {
-                        arg = 1;
-                    }
-                    else if (strcmp(istr, "RU") == 0) {
-                        arg = 2;
-                    }
-                    else {
-                        arg = 0;
-                    }
-                printf("Argument it's; %d\n", arg);  
+            choice = '4'; //Нинтендо свич
+            //Сравнение строки с аргументами
+            if (istr != NULL) {
+                if (strcmp(istr, "EN") == 0) {
+                    arg = 1; //Присвоение
+                } else if (strcmp(istr, "RU") == 0) {
+                    arg = 2; //Аргумента
+                } else {
+                    arg = 0; //Это тоже оно
                 }
-            printf("4\n");
+                printf("Argument it's; %d\n", arg); //Вспомогательная строка
+            }
+            printf("4\n"); //Вспомогательная строка
         }
+        //Сравнение содержимого строки с командой
         else if (strcmp(istr1, "stats") == 0) {
-            if ((istr != NULL) && (isnumber(istr) == 0)) { 
-                argtest = atoi(istr);
+            if ((istr != NULL) && (isnumber(istr) == 0)) {
+                argtest = atoi(istr); //Перевод аргумента из строки в int
                 if ((argtest >= 1) && (argtest <= 100)) {
-                    arg = argtest;
-                    printf("Argument it's; %d\n", arg);
+                    arg = argtest; //Присвоение аргумента
+                    printf("Argument it's; %d\n", arg); //Вспомогательная строка
                 } else {
                     printf("Invalid argument!\n");
-                    arg = 9;
+                    arg = 9; //Дефолтное значение аргумента из ТЗ
                 }
             }
-            choice = '5';
-            printf("5\n");
-        }
-        else if (strcmp(istr1, "stats_export") == 0) {
-            choice = '6';
-            printf("6\n");
-        }
-        else if (strcmp(istr1, "stats_delete") == 0) {
-            choice = '7';
-            printf("7\n");
-        }
-        else if (strcmp(istr1, "quit") == 0) {
-            choice = '8';
-            printf("8\n");
-        }
-        else {
+            choice = '5';  //Свичкейс
+            printf("5\n"); //Вспомогательная строка
+            //Сравнение содержимого строки с командой
+        } else if (strcmp(istr1, "stats_export") == 0) {
+            choice = '6';  //Я усталб((9(
+            printf("6\n"); //Вспомогательная строка
+            //Сравнение содержимого строки с командой
+        } else if (strcmp(istr1, "stats_delete") == 0) {
+            choice = '7';  //Свичкейс
+            printf("7\n"); //Вспомогательная строка
+            //Сравнение содержимого строки с командой
+        } else if (strcmp(istr1, "quit") == 0) {
+            choice = '8';  //Свичкейс
+            printf("8\n"); //Вспомогательная строка
+            //Если введенной команды не сущесвтует
+        } else {
             printf("Incorrent input!\n");
         }
         setlocale(LC_ALL, "");
         wchar_t** text;
-        
+        //Свичкейс нужно комментить?
         if (isdigit(choice)) {
             switch (choice) {
             case '1':
-                //text = texts_get(0);
-                //texts_print(text);
+                // text = texts_get(0);
+                // texts_print(text);
                 // texts_read(text);
-                 printf("a\n");
+                printf("a\n");
                 break;
             case '2':
-                //text = texts_get(practice());
-                //texts_print(text);
-                //texts_read(text);
+                // text = texts_get(practice());
+                // texts_print(text);
+                // texts_read(text);
                 printf("b\n");
                 break;
             case '3':
                 // practice_best();
                 printf("c\n");
                 break;
-            case '4': 
+            case '4':
                 // language();
                 printf("d\n");
                 break;
@@ -182,10 +186,11 @@ void menu(void)
              && choice != '5' && choice != '6' && choice != '7');
 }
 //Самописная функция для проверки числа в строке
-int isnumber(char* buf){
+int isnumber(char* buf)
+{
     char* cur = buf;
     while (*cur != '\0') {
-         if (isdigit(*cur) == 0) {
+        if (isdigit(*cur) == 0) {
             return -1;
         }
         cur++;
@@ -193,7 +198,8 @@ int isnumber(char* buf){
     return 0;
 }
 //Функция для тестирования файла
-int main() {
+int main()
+{
     menu_display();
     menu();
     return 0;
